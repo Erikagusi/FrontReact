1060c152bfbf6ab526b408f00522b027a36a1d9e
Erika <erika.guarin.s@gmail.com>
2023-10-31 10:39:01 -0500

    Examen Front 3

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './Card'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Carga de Estudiantes</h1>
+      <form></form>
+      <Card/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        Hola, Soy una Tarjeta
+    </div>
+  )
+}
+
+export default Card
