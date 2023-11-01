0413502af234252971b503afb27b4a6d263ef497
Erika <erika.guarin.s@gmail.com>
2023-11-01 18:09:10 -0500

    Examen Front 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
 import './App.css'
 import Card from './Card'
+import Style from './style.module.css'
+import { useState } from 'react'
+
 
 
 function App() {
+  const [persona, setPersona] = useState({
+    nombre: '',
+    autor: '',
+    libro: ''})
+  const [error, setError] = useState(false);
+  const [bueno, setBueno] = useState(false)
+
+  const handleSumbit = (e) => {
+    e.preventDefault()
+    if ((persona.nombre.length > 3 && persona.nombre[0] !==" ") && (persona.libro.length > 5)) {
+      setError(false)
+      setBueno(true)
+    } else {
+      setError(true)
+      setBueno(false)
+    }
+
+  }
 
   return (
     <>
-      <h1>Carga de Estudiantes</h1>
-      <form></form>
-      <Card/>
+      <div>
+        <h1 className={Style.h1}>Libro Favorito</h1>
+        {!bueno &&
+          <form className={Style.form} onSubmit={handleSumbit}>
+            <input type="text" value={persona.nombre} placeholder='Ingrese su Nombre' onChange={(e) => setPersona({ ...persona, nombre: e.target.value })} />
+            <br />
+            <input type="text" value={persona.libro} placeholder='Ingrese su libro Favorito' onChange={(e) => setPersona({ ...persona, libro: e.target.value })} />
+            <br />
+            <input type="text" value={persona.anios} placeholder='Ingresa el autor' onChange={(e) => setPersona({ ...persona, autor: e.target.value })} />
+            <br />
+
+            <button className={Style.button}> Enviar </button>
+          </form>}
+
+
+        {bueno && <Card nombre={persona.nombre} libro={persona.libro} autor={persona.autor} />}
+        {error && <h4>Por favor chequea que la información sea correcta</h4>}
+      </div>
 
     </>
   )
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Style from './style.module.css'
 
-const Card = () => {
+const Card = ({nombre, libro, autor}) => {
   return (
-    <div>
-        Hola, Soy una Tarjeta
+    <div className={Style.divCard}>
+      <h3>Hola {nombre}</h3>
+      <h4>Tu libro Favorito es</h4>
+      <h3>{libro}</h3>
+      <h4>El autor del libro {libro} es {autor} </h4>
     </div>
   )
 }
